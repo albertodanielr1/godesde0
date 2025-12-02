f797ec3e0ec248e84a89a96d8772635fdb5e508d
Alberto Reina <albertodanielr1@gmail.com>
2025-12-01 21:31:28 -0400

    mi primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/albertodanielr1/godesde0
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/albertodanielr1/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
